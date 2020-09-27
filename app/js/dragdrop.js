de4248d79fd9f8f5db4e5ae601784c5dcbc301b5
Brian Harper <bharper@gmail.com>
2020-09-27 16:50:23 -0500

    removed some commented out code

--- app/js/dragdrop.js
+++ app/js/dragdrop.js
@@ -1,25 +1,4 @@
-//['ondragover', 'ondragleave', 'ondragend', 'ondrop']
-
-
 var counter = 0;
-// var showing = false;
-// function _hideDroptarget() {
-//     if (! showing) return;
-//     console.log('drag end')
-//     showing = false;
-//     $droptarget.hide();
-// }
-
-// function _showDroptarget() {
-//     if (! showing) {
-//         console.log('drag start');
-//         showing = true;
-//     }
-//     $droptarget.show();
-// }
-
-// var hideDroptarget = _.debounce(_hideDroptarget, 200);
-// var showDroptarget = _.debounce(_showDroptarget, 200);
 
 $(function () {
     var $droptarget = $("#droptarget");
@@ -29,7 +8,6 @@ $(function () {
             $("#yturl").val("");
             console.log('showing target');
             $droptarget[0].style.display = "flex";
-            //$droptarget.show();
         }
         e.preventDefault();
         e.stopPropagation();
