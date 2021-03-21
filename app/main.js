4b5b4ed64eef55dbbf007a849bad9bb416fa76b2
Brian Harper <bharper@gmail.com>
2021-03-21 01:15:40 -0500

    Fixed new context isolation default

--- app/main.js
+++ app/main.js
@@ -1,28 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 app.allowRendererProcessReuse = false;
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 500,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
-    }
-  })
-  //preload: path.join(__dirname, 'preload.js')
-  if (process.env['DEV']) mainWindow.openDevTools({detached:true})
-  mainWindow.setMenuBarVisibility(false)
-  mainWindow.loadFile('index.html')
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+            width: 800,
+            height: 500,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true,
+                contextIsolation: false
+            }
+        })
+        //preload: path.join(__dirname, 'preload.js')
+    if (process.env['DEV']) mainWindow.openDevTools({ detached: true })
+    mainWindow.setMenuBarVisibility(false)
+    mainWindow.loadFile('index.html')
 
-  
+
 }
 
 app.whenReady().then(createWindow)
 
-app.on('activate', function () {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.on('activate', function() {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
-
